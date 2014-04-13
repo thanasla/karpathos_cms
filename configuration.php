6998290a333dc731469076030c720115d20783ac
thanasla <thanasis777@msn.com>
2014-04-13 12:08:54 +0300

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,14 +1,10 @@
 <?php 
-$dbhost2="db25.grserver.gr";
-$dbuser2="thanasis";
-$dbpass2="thanasis";
-$dbname2="karpathos";
 
 
 $dbhost="localhost";
 $dbuser="root";
-$dbpass="thanal2106";
-$dbname="test";
+$dbpass="";
+$dbname="";
 
 
 
